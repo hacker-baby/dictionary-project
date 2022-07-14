43c0ce63923216bcce5b1ab96d87fc9a4e358c63
hckrbaby <taiselize@gmail.com>
2022-07-14 17:58:57 +0200

    Created search engine

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import logo from "./app-logo.png";
+import Dictionary from "./Dictionary";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo img-fluid" alt="logo" />
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">Coded by Tais Elize</footer>
+      </div>
     </div>
   );
 }
