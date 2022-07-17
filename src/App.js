711c08cc2ed025c84c455ed606d11ebec5fc6245
hckrbaby <taiselize@gmail.com>
2022-07-17 16:16:54 +0200

    Added font + updated images and styling

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import logo from "./app-logo.png";
+import social from "./social-img.png";
 import Dictionary from "./Dictionary";
 import "./App.css";
 
@@ -12,7 +13,30 @@ export default function App() {
         <main>
           <Dictionary />
         </main>
-        <footer className="App-footer">Coded by Tais Elize</footer>
+        <div className="social">
+          <a
+            href="https://www.instagram.com/hacker___baby/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img
+              src={social}
+              className="Social-img img-fluid"
+              alt="social-link"
+            />
+          </a>
+        </div>
+        <footer className="App-footer">
+          This is an{" "}
+          <a
+            href="https://github.com/hacker-baby/dictionary-project"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-source
+          </a>{" "}
+          project built by Tais Elize.
+        </footer>
       </div>
     </div>
   );
